06f2247391be740cedbd2f7b21f76e3d51876b97
AwrenNuit <a.rennaker@yahoo.com>
2020-06-13 15:49:38 -0500

    basic component setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1>[Safe Healthcare]</h1>
+      <p>
+        People who hold oppressed or marginalized identities often experience
+        significant discrimination from healthcare workers. This results in
+        worse overall healthcare, less visits to the doctor, and sometimes
+        traumatic experiences or death. The system as it exists simply will not
+        do. The goal of [Safe Healthcare] is to make it easier for people who
+        hold oppressed or marginalized identities to find a healthcare
+        practitioner that will provide better care.
+      </p>
+      <p>
+        Accounts are not required for basic use like viewing, rating, and
+        reviewing doctors. However, an account is required to track Favorites.
+      </p>
+      <p>Search your area for safer healthcare now.</p>
+      <div>
+        <button>SEARCH!</button>
+      </div>
+    </>
+  );
+}
