3e5d7c24c3c1247ec435f718feed0f8a1632d889
AwrenNuit <a.rennaker@yahoo.com>
2020-06-13 19:14:00 -0500

    updated heading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <h1>[Safe Healthcare]</h1>
+      <h1>Reimagining Healthcare</h1>
       <p>
         People who hold oppressed or marginalized identities often experience
         significant discrimination from healthcare workers. This results in
         worse overall healthcare, less visits to the doctor, and sometimes
         traumatic experiences or death. The system as it exists simply will not
-        do. The goal of [Safe Healthcare] is to make it easier for people who
+        do. The goal of [Safer Healthcare] is to make it easier for people who
         hold oppressed or marginalized identities to find a healthcare
         practitioner that will provide better care.
       </p>
