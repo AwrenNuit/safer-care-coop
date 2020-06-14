0e78606c56acfd13caf93d13227b0a48930be2bb
AwrenNuit <a.rennaker@yahoo.com>
2020-06-13 19:30:12 -0500

    set up Header component with basic styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
-
-  return(
-    <>
-    </>
+  return (
+    <div className="main-container">
+      <div id="header">
+        <h2>[Safer Healthcare]</h2>
+      </div>
+      {/* add icon to searchbar */}
+      <input
+        id="searchbar"
+        type="text"
+        placeholder="search healthcare practitioners"
+      />
+    </div>
   );
 }
