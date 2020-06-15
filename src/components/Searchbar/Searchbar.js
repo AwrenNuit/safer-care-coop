31f8e6334ed8862a373abb4b05615ad4725088de
AwrenNuit <a.rennaker@yahoo.com>
2020-06-14 19:56:54 -0500

    minor formatting updates

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './Searchbar.css';
-import Checkbox from '../Checkbox/Checkbox';
+import React, { useState } from "react";
+import "./Searchbar.css";
+import Checkbox from "../Checkbox/Checkbox";
 
 export default function Searchbar() {
   const [GP, setGP] = useState(false);
@@ -9,24 +9,22 @@ export default function Searchbar() {
   const [trans, setTrans] = useState(false);
   const [women, setWomen] = useState(false);
   const [checkboxes, setCheckboxes] = useState([
-    { setting: setGP, label: 'General Practitioner' },
-    { setting: setTherapist, label: 'Therapist' },
-    { setting: setPOC, label: 'POC' },
-    { setting: setTrans, label: 'Transgender' },
-    { setting: setWomen, label: 'Women' }
+    { setting: setGP, label: "General Practitioner" },
+    { setting: setTherapist, label: "Therapist" },
+    { setting: setPOC, label: "POC" },
+    { setting: setTrans, label: "Transgender" },
+    { setting: setWomen, label: "Women" },
   ]);
 
-  return(
-    <div id="search-container">   
+  return (
+    <div id="search-container">
       {/* add icon to searchbar */}
-      <input
-        id="searchbar"
-        type="text"
-        placeholder="enter zipcode or city"
-      />
+      <input id="searchbar" type="text" placeholder="enter zipcode or city" />
       <button id="search-btn">SEARCH</button>
       {/* set each checkbox to grid */}
-      {checkboxes.map((item, i) => <Checkbox key={i} label={item.label} setting={item.setting} />)}
+      {checkboxes.map((item, i) => (
+        <Checkbox key={i} label={item.label} setting={item.setting} />
+      ))}
     </div>
   );
 }
