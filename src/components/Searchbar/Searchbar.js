2b1616fe2ece93e1c186e312d5f9ba59f483bdef
AwrenNuit <a.rennaker@yahoo.com>
2020-06-14 20:01:21 -0500

    SEARCH button pushes to /results

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./Searchbar.css";
 import Checkbox from "../Checkbox/Checkbox";
+import { useHistory } from "react-router-dom";
 
 export default function Searchbar() {
+  const history = useHistory();
   const [GP, setGP] = useState(false);
   const [therapist, setTherapist] = useState(false);
   const [POC, setPOC] = useState(false);
@@ -16,11 +18,18 @@ export default function Searchbar() {
     { setting: setWomen, label: "Women" },
   ]);
 
+  const postSearch = () => {
+    // generate search based on local state filters and zipcode/city
+    history.push("/results");
+  };
+
   return (
     <div id="search-container">
       {/* add icon to searchbar */}
       <input id="searchbar" type="text" placeholder="enter zipcode or city" />
-      <button id="search-btn">SEARCH</button>
+      <button id="search-btn" onClick={postSearch}>
+        SEARCH
+      </button>
       {/* set each checkbox to grid */}
       {checkboxes.map((item, i) => (
         <Checkbox key={i} label={item.label} setting={item.setting} />
