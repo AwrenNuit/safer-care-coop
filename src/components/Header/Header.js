b0c6a368024c74887809f1b19a31cbec0cb817cc
AwrenNuit <a.rennaker@yahoo.com>
2020-06-14 19:31:07 -0500

    clicking header sends user to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./Header.css";
 import Searchbar from "../Searchbar/Searchbar";
 
 export default function Header() {
+  const history = useHistory();
+  
   return (
     <div id="header-container">
-      <div id="header">
+      <div id="header" onClick={() => history.push('/')}>
         <h2>[Safer Healthcare]</h2>
       </div>
       <Searchbar />
