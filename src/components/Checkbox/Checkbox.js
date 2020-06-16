06d3e6280318427def8b1867d36b210872602c9e
AwrenNuit <a.rennaker@yahoo.com>
2020-06-16 13:36:44 -0500

    deactivated search input, checkboxes adhere to a grid layout

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function Checkbox(props) {
   return (
     // set each checkbox to grid
-    <div style={{ display: "inline-block", marginRight: "1rem" }}>
+    <div style={{ display: "inline-block", margin: "0 1rem 0.5rem 0" }}>
       <label>
         <input
+        style={{marginRight: "0.5rem"}}
           type="checkbox"
           onChange={(e) => props.setting(e.target.checked)}
         />
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -10,12 +10,14 @@ export default function Searchbar() {
   const [POC, setPOC] = useState(false);
   const [trans, setTrans] = useState(false);
   const [women, setWomen] = useState(false);
+  const [placeholder, setPlaceholder] = useState(false);
   const [checkboxes, setCheckboxes] = useState([
     { setting: setGP, label: "General Practitioner" },
     { setting: setTherapist, label: "Therapist" },
     { setting: setPOC, label: "POC" },
     { setting: setTrans, label: "Transgender" },
     { setting: setWomen, label: "Women" },
+    { setting: setPlaceholder, label: "Placeholder" },
   ]);
 
   const postSearch = () => {
@@ -26,11 +28,13 @@ export default function Searchbar() {
   return (
     <div id="search-container">
       {/* add icon to searchbar */}
-      <input id="searchbar" type="text" placeholder="enter zipcode or city" />
-      {/* set each checkbox to grid */}
-      {checkboxes.map((item, i) => (
-        <Checkbox key={i} label={item.label} setting={item.setting} />
-      ))}
+      {/* searchbar will only be available once zipcode logic is implemented */}
+      {/* <input id="searchbar" type="text" placeholder="enter zipcode or city" /> */}
+      <div id="filter-grid">
+        {checkboxes.map((item, i) => (
+          <Checkbox key={i} label={item.label} setting={item.setting} />
+        ))}
+      </div>
       <button id="search-btn" onClick={postSearch}>
         SEARCH
       </button>
