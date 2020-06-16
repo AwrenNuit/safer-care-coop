a760825062fa077ca7ef0cdeba833fabf6b31e5e
AwrenNuit <a.rennaker@yahoo.com>
2020-06-16 18:34:26 -0500

    minor formatting updates

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -2,11 +2,10 @@ import React from "react";
 
 export default function Checkbox(props) {
   return (
-    // set each checkbox to grid
     <div style={{ display: "inline-block", margin: "0 1rem 0.5rem 0" }}>
       <label>
         <input
-        style={{marginRight: "0.5rem"}}
+          style={{ marginRight: "0.5rem" }}
           type="checkbox"
           onChange={(e) => props.setting(e.target.checked)}
         />
