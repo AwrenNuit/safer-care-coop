3dd485234b6bb98e4531640a151529a16a5a74c2
AwrenNuit <a.rennaker@yahoo.com>
2020-06-15 19:11:36 -0500

    layout rough draft

--- src/components/SelectedPractitioner/SelectedPractitioner.js
+++ src/components/SelectedPractitioner/SelectedPractitioner.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import './SelectedPractitioner.css';
 
 export default function SelectedPractitioner() {
+  // const { state, dispatch } = useContext(selectedPractitionerContext);
+
   return (
-    <>
-      <p>made it</p>
-    </>
+    <div className="main-container">
+      {/* <h1>{state.name}</h1>
+      <div>
+        <img src={state.img} alt="" />
+        <div>
+          <p>{state.employer}</p>
+          <div>{state.rating}</div>
+          <p>{state.bio}</p>
+          <p>Tags: {state.tags.map((tag, i) => <span>{tag}</span>)}</p>
+        </div>
+      </div> */}
+      <h1 style={{textAlign:"center"}}>Doctor 1</h1>
+      <div id="selected-grid">
+        <img id="selected-img" src="/images/doc.png" alt="" />
+        <div>
+          <p>Employer</p>
+          <div>3/5 stars</div>
+          <p>Here's a brief bio about this healthcare practitioner. They mostly do this type of work.</p>
+          <p>Tags: <span>Transgender </span><span>POC </span><span>Women </span></p>
+        </div>
+      </div>
+    </div>
   );
 }
