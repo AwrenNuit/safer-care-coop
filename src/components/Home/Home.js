8fa3ca8a275413d7dc78ad8905572a054d643593
AwrenNuit <a.rennaker@yahoo.com>
2020-06-17 10:10:45 -0500

    minor formatting updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,9 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="main-container">
-      <h1>Reimagining Healthcare</h1>
+      <h1 style={{ marginBottom: "1rem", textAlign: "center" }}>
+        Making Healthcare Safer
+      </h1>
       <p>
         People who hold oppressed or marginalized identities often experience
         significant discrimination from healthcare workers. This results in
@@ -14,8 +16,6 @@ export default function Home() {
         hold oppressed or marginalized identities to find a healthcare
         practitioner that will provide better care.
       </p>
-      <br />
-      <p id="home-bold">Search your area for safer healthcare now</p>
     </div>
   );
 }
