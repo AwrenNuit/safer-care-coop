6310773537877085b831e67b27e66d81dcc73960
AwrenNuit <a.rennaker@yahoo.com>
2020-06-19 15:30:47 -0500

    minor formatting updates

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import "./Searchbar.css";
 import Checkbox from "../Checkbox/Checkbox";
 import { useHistory } from "react-router-dom";
