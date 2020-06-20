b0af35cd9d122ad69a5238b3719124d1b2076dc9
AwrenNuit <a.rennaker@yahoo.com>
2020-06-20 18:54:50 -0500

    reviews are mapped through and rendered to the dom

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
 import StarRating from "react-star-ratings";
 
-export default function Reviews() {
+export default function Reviews(props) {
   return (
-    // this data will be mapped through
-    <div
-      style={{
-        border: "3px solid black",
-        marginTop: "1.5rem",
-        padding: "1rem",
-      }}
-    >
-      <div style={{ float: "right" }}>
-        <StarRating
-          name="rating"
-          numberOfStars={5}
-          rating={3} // use actual review rating
-          starDimension="30px"
-          starRatedColor="gold"
-          starSpacing="0"
-        />
-      </div>
-      <p>name (or default anonymous)</p>
-      <p>date</p>
-      <br />
-      <p>
-        This is where the review of this practitioner goes. How are they? Did I
-        enjoy my visit? Would I recommend them to someone else? Were they great?
-        Terrible? This space is where I let the world know.
-      </p>
-    </div>
+    <>
+      {Object.entries(props.reviews).flat(Infinity).map((item, i) => 
+      <Fragment key={i}>
+        {item.review ?
+          <div
+            style={{
+              border: "3px solid black",
+              marginTop: "1.5rem",
+              padding: "1rem",
+            }}
+          >
+            <div style={{ float: "right" }}>
+              <StarRating
+                name="rating"
+                numberOfStars={5}
+                rating={item.rating}
+                starDimension="30px"
+                starRatedColor="gold"
+                starSpacing="0"
+              />
+            </div>
+            <p>{item.name}</p>
+            <p>{item.date}</p>
+            <br />
+            <p>{item.review}</p>
+          </div>
+        :
+        ''
+        }
+      </Fragment>
+      )}
+    </>
   );
 }
