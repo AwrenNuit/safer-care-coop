6de167ed460834fba742ea6e6bc242ee66724aeb
AwrenNuit <a.rennaker@yahoo.com>
2020-06-20 18:55:09 -0500

    minor formatting updates

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -5,34 +5,34 @@ export default function Reviews(props) {
   return (
     <>
       {Object.entries(props.reviews).flat(Infinity).map((item, i) => 
-      <Fragment key={i}>
-        {item.review ?
-          <div
-            style={{
-              border: "3px solid black",
-              marginTop: "1.5rem",
-              padding: "1rem",
-            }}
-          >
-            <div style={{ float: "right" }}>
-              <StarRating
-                name="rating"
-                numberOfStars={5}
-                rating={item.rating}
-                starDimension="30px"
-                starRatedColor="gold"
-                starSpacing="0"
-              />
+        <Fragment key={i}>
+          {item.review ?
+            <div
+              style={{
+                border: "3px solid black",
+                marginTop: "1.5rem",
+                padding: "1rem",
+              }}
+            >
+              <div style={{ float: "right" }}>
+                <StarRating
+                  name="rating"
+                  numberOfStars={5}
+                  rating={item.rating}
+                  starDimension="30px"
+                  starRatedColor="gold"
+                  starSpacing="0"
+                />
+              </div>
+              <p>{item.name}</p>
+              <p>{item.date}</p>
+              <br />
+              <p>{item.review}</p>
             </div>
-            <p>{item.name}</p>
-            <p>{item.date}</p>
-            <br />
-            <p>{item.review}</p>
-          </div>
-        :
-        ''
-        }
-      </Fragment>
+          :
+          ''
+          }
+        </Fragment>
       )}
     </>
   );
