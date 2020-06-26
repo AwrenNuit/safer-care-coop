a0b7d192def768deaf15dbf337ae688a176c296c
AwrenNuit <a.rennaker@yahoo.com>
2020-06-26 15:47:08 -0500

    minor formatting updates

--- src/components/AddPractitioner/Input.js
+++ src/components/AddPractitioner/Input.js
@@ -3,26 +3,26 @@ import React from "react";
 export default function Input(props) {
   return (
     <div>
-      {props.type === 'text' ?
-      <>
-        <label>{props.label}</label>
-        <input
-          onChange={(e) => props.setValue(e.target.value)}
-          placeholder={props.placeholder}
-          type={props.type}
-          value={props.value}
-        />
-      </>
-      :
-      <label>
-        <input
-          onChange={e => props.setValue(e.target.checked)}
-          type={props.type}
-          value={props.value}
+      {props.type === "text" ? (
+        <>
+          <label>{props.label}</label>
+          <input
+            onChange={(e) => props.setValue(e.target.value)}
+            placeholder={props.placeholder}
+            type={props.type}
+            value={props.value}
           />
-        {props.label}
-      </label>
-      }
+        </>
+      ) : (
+        <label>
+          <input
+            onChange={(e) => props.setValue(e.target.checked)}
+            type={props.type}
+            value={props.value}
+          />
+          {props.label}
+        </label>
+      )}
     </div>
   );
 }
--- src/components/AddPractitioner/NewPractitioner.js
+++ src/components/AddPractitioner/NewPractitioner.js
@@ -16,99 +16,105 @@ export default function NewPractitioner() {
   const [women, setWomen] = useState(false);
   const [starRating, setStarRating] = useState(3);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("asdasdasd");
+  };
   return (
     <>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <h1>Add-a-Doc</h1>
         <div>
-          <Input
-            label="Practitioner Name: "
-            placeholder="name"
-            setValue={setName}
-            type="text"
-            value={name}
-          />
-          <Input
-            label="Employer: "
-            placeholder="employer"
-            setValue={setEmployer}
-            type="text"
-            value={employer}
-          />
-          <Input
-            label="Phone Number: "
-            placeholder="xxx-xxx-xxxx"
-            setValue={setPhoneNumber}
-            type="text"
-            value={phoneNumber}
-          />
-          <Input
-            label="Picture: "
-            placeholder="image url"
-            setValue={setImage}
-            type="text"
-            value={image}
-          />
-          <Input
-            label="Background & Services: "
-            placeholder="background"
-            setValue={setBio}
-            type="text"
-            value={bio}
-          />
+          <div>
+            <Input
+              label="Practitioner Name: "
+              placeholder="name"
+              setValue={setName}
+              type="text"
+              value={name}
+            />
+            <Input
+              label="Employer: "
+              placeholder="employer"
+              setValue={setEmployer}
+              type="text"
+              value={employer}
+            />
+            <Input
+              label="Phone Number: "
+              placeholder="xxx-xxx-xxxx"
+              setValue={setPhoneNumber}
+              type="text"
+              value={phoneNumber}
+            />
+            <Input
+              label="Picture: "
+              placeholder="image url"
+              setValue={setImage}
+              type="text"
+              value={image}
+            />
+            <Input
+              label="Background & Services: "
+              placeholder="background"
+              setValue={setBio}
+              type="text"
+              value={bio}
+            />
+          </div>
+          <div>
+            <p>Tags</p>
+            <Input
+              label="Physician"
+              setValue={setPhysician}
+              type="checkbox"
+              value={physician}
+            />
+            <Input
+              label="Therapist"
+              setValue={setTherapist}
+              type="checkbox"
+              value={therapist}
+            />
+            <Input label="POC" setValue={setPOC} type="checkbox" value={POC} />
+            <Input
+              label="Transgender"
+              setValue={setTransgender}
+              type="checkbox"
+              value={transgender}
+            />
+            <Input
+              label="Women"
+              setValue={setWomen}
+              type="checkbox"
+              value={women}
+            />
+            <Input
+              label="Queer"
+              setValue={setQueer}
+              type="checkbox"
+              value={queer}
+            />
+          </div>
 
-          <p>Tags</p>
-          <Input
-            label="Physician"
-            setValue={setPhysician}
-            type="checkbox"
-            value={physician}
-          />
-          <Input
-            label="Therapist"
-            setValue={setTherapist}
-            type="checkbox"
-            value={therapist}
-          />
-          <Input
-            label="POC"
-            setValue={setPOC}
-            type="checkbox"
-            value={POC}
-          />
-          <Input
-            label="Transgender"
-            setValue={setTransgender}
-            type="checkbox"
-            value={transgender}
-          />
-          <Input
-            label="Women"
-            setValue={setWomen}
-            type="checkbox"
-            value={women}
-          />
-          <Input
-            label="Queer"
-            setValue={setQueer}
-            type="checkbox"
-            value={queer}
-          />
-
-          <br />
-          <p>Your Rating & Review (optional but helpful!)</p>
-          <StarRating
-            changeRating={(rating) => setStarRating(rating)}
-            isSelectable={true}
-            name="rating"
-            numberOfStars={5}
-            rating={starRating}
-            starDimension="30px"
-            starHoverColor="gold"
-            starRatedColor="gold"
-            starSpacing="0"
-          />
-          <input type="text" placeholder="your review" />
+          <div>
+            <p>Your Rating & Review (optional but helpful!)</p>
+            <StarRating
+              changeRating={(rating) => setStarRating(rating)}
+              isSelectable={true}
+              name="rating"
+              numberOfStars={5}
+              rating={starRating}
+              starDimension="30px"
+              starHoverColor="gold"
+              starRatedColor="gold"
+              starSpacing="0"
+            />
+            <input type="text" placeholder="your review" />
+          </div>
+          <div>
+            <button type="submit">SUBMIT</button>
+          </div>
         </div>
       </form>
     </>
