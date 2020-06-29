f6d463e69dbb4aee279712c092777a988d3348f3
AwrenNuit <a.rennaker@yahoo.com>
2020-06-29 15:13:28 -0500

    required textfields are red if not filled out

--- src/components/AddPractitioner/Input.js
+++ src/components/AddPractitioner/Input.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 export default function Input(props) {
+  const setStyle = () => {
+    if (!props.required) {
+      return;
+    } else {
+      if (props.label === "Phone Number" && props.value.length !== 12) {
+        return { color: "red" };
+      } else if (props.label !== "Phone Number" && props.value.length === 0) {
+        return { color: "red" };
+      } else {
+        return;
+      }
+    }
+  };
   return (
     <>
       {props.type === "text" ? (
         <div style={{ marginTop: "20px" }}>
-          <label>
+          <label style={setStyle()}>
             {props.required ? `${props.label} (required)` : props.label}
           </label>
           <br />
@@ -18,16 +31,24 @@ export default function Input(props) {
           />
         </div>
       ) : (
-        <label className={props.type === 'checkbox' ? 'checkbox-container' : 'radio-container'}>
+        <label
+          className={
+            props.type === "checkbox" ? "checkbox-container" : "radio-container"
+          }
+        >
           {props.label}
           <input
-            className={props.type === 'checkbox' ? 'checkbox' : 'radio'}
+            className={props.type === "checkbox" ? "checkbox" : "radio"}
             name={props.name || null}
             onChange={(e) => props.handleChange(e.target.checked, props.label)}
             type={props.type}
             value={props.value}
           />
-          <span className={props.type === 'checkbox' ? 'checkbox-check' : 'radio-active'}></span>
+          <span
+            className={
+              props.type === "checkbox" ? "checkbox-check" : "radio-active"
+            }
+          ></span>
         </label>
       )}
     </>
